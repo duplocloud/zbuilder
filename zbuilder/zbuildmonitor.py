46c5af1ba04e77facf6710e7360c4e604a270238
Srikar Naramsetty <srikar@duplocloud.net>
2024-02-19 19:56:05 +0530

    ubuntu22 +python3

--- zbuilder/zbuildmonitor.py
+++ zbuilder/zbuildmonitor.py
@@ -5,83 +5,80 @@ import os.path
 import traceback
 import subprocess
 import time
-import boto
-from boto.s3.key import Key
+import boto3
+
+from boto3.s3.transfer import S3Transfer
 
 def s3Upload(targetFile, srcFile):
     lFileExists = os.path.isfile(srcFile) 
-    print 'Call to upload ' + srcFile + ' toFile ' + targetFile
-    if lFileExists != True:
-        print '**** Requested file does not exist yet'
+    print ('Call to upload ' + srcFile + ' toFile ' + targetFile)
+    if not lFileExists:
+        print ('**** Requested file does not exist yet')
         return
     targetFile = os.getenv('GIT_REPO') + '/' + targetFile
     bucketName = os.getenv('S3_BUCKET')
-    conn = boto.connect_s3()
-    b = conn.get_bucket(bucketName)
-    k = Key(b)
-    k.key = targetFile
-    k.set_contents_from_filename(srcFile)
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file(srcFile, bucketName, targetFile)
 
 def uploadLogs(aInRunning):
-    print 'Begin log upload'
+    print ('Begin log upload')
     logFileName = os.getenv('LOG_FILE')
     logFileName = logFileName + '_' + os.getenv('REPLICA_ID')
     # Upload the files
     s3Upload(logFileName, '/zbuild.log') 
     
-    if aInRunning == False:
+    if not aInRunning:
         logFileName = '/' + logFileName + '.complete'
         s3Upload(logFileName, logFileName)
     
 
 def isBuildRunning():
-    val = subprocess.check_output(["ps", "-ax"])
-    lStatus = val.decode("utf-8")
-    lProcs = lStatus.splitlines()
+    val = subprocess.check_output(["ps", "-ax"]).decode("utf-8")
+    lProcs = val.splitlines()
     lRunning = False
     for lProc in lProcs:
         if 'zbuild.sh' in lProc:
-            print "Build is still running"
+            print ("Build is still running")
             lRunning = True
             break
-    if lRunning == False:
-        print 'Build is not running'
+    if not lRunning:
+        print ('Build is not running')
  
     return lRunning
        
 def launchBuild():
-   os.system("/bin/bash /zbuild.sh &") 
+   subprocess.Popen(["/bin/bash", "/zbuild.sh"])
 
     
 def main():
-    print "Starting Build Process"
+    print ("Starting Build Process")
     lRunning = isBuildRunning()
 
-    if lRunning == False:
-        print 'Starting new build'
+    if not lRunning:
+        print ('Starting new build')
         launchBuild()
     else:
-        print 'Old build is running'
+        print ('Old build is running')
  
-    print "Starting build monitor"
+    print ("Starting build monitor")
     #import pdb; pdb.set_trace()
     lRunning = False
-    while(True):
+    while True:
         time.sleep(10)
         
         try:
             lRunning = isBuildRunning()
             uploadLogs(lRunning)
-            if lRunning == False:
+            if not lRunning:
                 break
-        except Exception, e:
+        except Exception as e:
             # import pdb; pdb.set_trace()
             print ("********************************************* UpdateLogs encountered an exception", sys.exc_info()[0])
-        print '=============================================== Updatelogs Completed'
+        print ('=============================================== Updatelogs Completed')
      
-    print 'No more log upload go to idle state'
+    print ('No more log upload go to idle state')
     # Wait for the master to kill	      
-    while(True):
+    while True:
         time.sleep(60)
 
 
